0bb06ef925d04307f9cc16f55b235ad52681e426
huangchangpin <840411321@qq.com>
2018-05-24 17:24:16 +0800

    Initial commit

---
+++ src/main/java/com/test/test.java
@@ -0,0 +1,28 @@
+package com.test;
+
+public class test {
+    private  String name;
+
+    public test(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "test{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public test() {
+        super();
+    }
+}
